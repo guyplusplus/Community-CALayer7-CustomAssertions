91adffebb3a458b2e8cc37420fdfa8260a037212
guyplusplus <>
2018-07-22 08:48:14 +0700

    fixed some assert and added cases

--- NoDuplicateJSONName/src/test/java/community/layer7/customassertion/noDuplicateJSONName/logic/JSONDuplicatedNameCheckerTest.java
+++ NoDuplicateJSONName/src/test/java/community/layer7/customassertion/noDuplicateJSONName/logic/JSONDuplicatedNameCheckerTest.java
@@ -18,27 +18,46 @@ public class JSONDuplicatedNameCheckerTest {
 	public void testChecks() {    	
     	try {
     		JSONDuplicatedNameChecker.checkDuplicatedName("{\"a\":123, \"a2\":456, \"b\":[1,2,3], \"c\":[4,{\"c\":22,\"d\":{\"e\":44,\"f\":88,\"e\":999}},5]}");
+    		assertTrue(false);
     	}
     	catch(DuplicatedKeyName e) {
     		assertEquals(e.getKeyName(), "$.c[1].d.e");
     	}
     	try {
     		JSONDuplicatedNameChecker.checkDuplicatedName("[9,{\"a\":123, \"a2\":456, \"b\":[1,2,3], \"c\":[4,{\"c\":22,\"d\":{\"e\":44,\"f\":88,\"e\":999}},5]}]");
+    		assertTrue(false);
     	}
     	catch(DuplicatedKeyName e) {
     		assertEquals(e.getKeyName(), "$.[1].c[1].d.e");
     	}
     	try {
     		JSONDuplicatedNameChecker.checkDuplicatedName("{\"a\":1,\"a\":2}");
+    		assertTrue(false);
     	}
     	catch(DuplicatedKeyName e) {
     		assertEquals(e.getKeyName(), "$.a");
     	}
     	try {
+    		//check if a1 and a\u0031 is the same
+    		JSONDuplicatedNameChecker.checkDuplicatedName("{\"a1\":1,\"a2\":2,\"a\\u0031\":3}");
+    		assertTrue(false);
+    	}
+    	catch(DuplicatedKeyName e) {
+    		assertEquals(e.getKeyName(), "$.a1");
+    	}
+    	try {
     		JSONDuplicatedNameChecker.checkDuplicatedName("[[1,2],[3,4],[5,{\"a\":1,\"a\":2}]]");
+    		assertTrue(false);
     	}
     	catch(DuplicatedKeyName e) {
     		assertEquals(e.getKeyName(), "$.[2][1].a");
+    	}
+    	try {
+    		JSONDuplicatedNameChecker.checkDuplicatedName("{\"a\":1,\"b\":[1,2,3,{\"a\":1,\"b\":2},4],\"a\":2}");
+    		assertTrue(false);
+    	}
+    	catch(DuplicatedKeyName e) {
+    		assertEquals(e.getKeyName(), "$.a");
     	}
 
     	try {
